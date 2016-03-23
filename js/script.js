f41d6a5a867b86e3b5566d385023a4ed1aacb422
Katrina Van Laan <kdvanlaan@gmail.com>
2016-03-22 20:59:21 -0500

    update

--- js/script.js
+++ js/script.js
@@ -1,88 +1,67 @@
-
-//Body
+//Query Selectors
 var body = document.querySelector("body")
+
 ///Color
 var colorChange = document.createElement("div")
 colorChange.setAttribute("id", "colorChange")
 body.appendChild(colorChange)
 
-/// Clock
+// Clock
 var date = document.createElement("div")
-
 body.appendChild(date)
-
 date.setAttribute("id", "clock")
 
-//phantom change 
-
-var changeDiv = document.createElement("div")
-
-body.appendChild(changeDiv)
-
-changeDiv.setAttribute("id", "change")
-
-/// Border
-
+//Border
 var borderContainer = document.createElement("div")
-
 borderContainer.setAttribute("id", "borderContainer")
-
 var border = document.createElement("div")
-
 border.setAttribute("id", "border")
-
 var borderStyles = window.getComputedStyle(border)
-
 body.appendChild(borderContainer)
 borderContainer.appendChild(border)
 
 
-
 ///Hex type Div
 var hex = document.createElement("div")
-
 hex.setAttribute("id", "hex")
-
 body.appendChild(hex)
 
-/// updateTime 
-  function updateTime() { 
+//end query selectors
 
-    var now = new Date();
-    var hours = now.getHours();
-    var minutes = now.getMinutes();
-    var seconds = now.getSeconds();
-    var currentTime = hours + ':' + minutes + ':' + seconds;    
-    var myClock = document.getElementById('clock');
-    
-    myClock.innerHTML = currentTime;
+// updateTime function
+function updateTime() {
 
-    // converting time into hex value
+    var now = new Date()
+    var hours = now.getHours()
+    var minutes = now.getMinutes()
+    var seconds = now.getSeconds()
+    var currentTime = hours + ':' + minutes + ':' + seconds
+    var myClock = document.getElementById('clock')
+
+    myClock.innerHTML = currentTime
+
+    //converting time to the hex value
     var hoursHex = hours.toString(16)
     var minutesHex = minutes.toString(16)
     var secondsHex = seconds.toString(16)
     var backgroundHex = '#' + hours + minutes + seconds
     hex.textContent = backgroundHex
+        //setting the background color to the hex value
     colorChange.style.background = "radial-gradient(circle," + backgroundHex + ",rgba(0,0,0,.5))"
-        
+
 }
 
+setInterval("updateTime()", 1000)
 
- setInterval("updateTime()", 1000)
-
-///// Border
+//Making the loading bar
 
 var width = border.style.width
 
-function updateTimeWidth() { 
+function updateTimeWidth() {
     var nowWidth = new Date()
     var secondsWidth = (nowWidth.getSeconds()) * 2
     border.style.width = secondsWidth + "px"
 }
-  
 
+//kick things off!
 setInterval("updateTimeWidth()", 2000)
-
-///
-
-
